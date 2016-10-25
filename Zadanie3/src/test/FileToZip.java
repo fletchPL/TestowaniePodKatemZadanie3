7191db26f053227b75be5ebb1b0d03c8a79c892a
bulikd105 <bulikd105@gmail.com>
2016-10-25 11:43:50 +0200

    Dodanie niezbednych deklaracji do pakowania pliku do zip

--- Zadanie3/src/test/FileToZip.java
+++ Zadanie3/src/test/FileToZip.java
@@ -1,6 +1,10 @@
 package test;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class FileToZip 
 {
@@ -13,5 +17,25 @@ public class FileToZip
 		this.zipName = zipName;
 	}
 	
-	
+	public void ConvertToZip(FileToZip fileToZip) throws IOException
+	{
+		byte[] buffer = new byte[1024];
+		
+		try
+		{
+			FileOutputStream fileOutputStream = new FileOutputStream(fileToZip.zipName);
+			ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
+			ZipEntry zipEntry = new ZipEntry(fileToZip.fileName);
+			
+			zipOutputStream.putNextEntry(zipEntry);
+			
+			FileInputStream fileInputStream = new FileInputStream(fileToZip.fileName);
+			
+			
+		}
+		finally
+		{
+			
+		}
+	}
 }
