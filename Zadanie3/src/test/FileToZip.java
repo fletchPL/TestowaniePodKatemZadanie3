f63a3f99908f559b6f206286be1efd284790d533
bulikd105 <bulikd105@gmail.com>
2016-10-25 11:39:45 +0200

    Przygotowanie konstruktora

--- Zadanie3/src/test/FileToZip.java
+++ Zadanie3/src/test/FileToZip.java
@@ -1,6 +1,17 @@
 package test;
 
+import java.io.IOException;
+
 public class FileToZip 
 {
-
+	String fileName = null;
+	String zipName = null;
+	
+	FileToZip(String fileName, String zipName)
+	{
+		this.fileName = fileName;
+		this.zipName = zipName;
+	}
+	
+	
 }
