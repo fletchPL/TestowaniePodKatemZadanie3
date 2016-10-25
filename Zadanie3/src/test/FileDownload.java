26640acbb45790a6b087ceb30895ef6380b24d9f
bulikd105 <bulikd105@gmail.com>
2016-10-25 11:28:05 +0200

    Przygotowanie konstruktora do pobierania pliku

--- Zadanie3/src/test/FileDownload.java
+++ Zadanie3/src/test/FileDownload.java
@@ -8,4 +8,21 @@ import java.net.URLConnection;
 
 public class FileDownload 
 {
+	URL url;
+	String fileName;
+	
+	FileDownload(URL url, String fileName)
+	{
+		this.url = url;
+		this.fileName = fileName;
+		
+		try 
+		{
+			Download(this);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
