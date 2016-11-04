b4b59d8d3a6b12e479c2ed771d4275b2cf2c323a
bulikd105 <bulikd105@gmail.com>
2016-11-04 10:06:53 +0100

    Added Decompressing method

--- Zadanie3/src/test/DecompressZipToFiles.java
+++ Zadanie3/src/test/DecompressZipToFiles.java
@@ -1,19 +1,70 @@
 package test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 public class DecompressZipToFiles 
 {
-	private String zipName = null;
-	private String outputFolder = null;
+	private String zipName = null; // Name of compressed zip file
+	private String outputFolder = null; // Path where to store decompressed files
 	
-	public DecompressZipToFiles()
+	public DecompressZipToFiles(String zipName, String outputFolder)
 	{
-		
+		this.zipName = zipName;
+		this.outputFolder = outputFolder;
 	}
 		
-			
-			
-	public void Decompress(DecompressZipToFiles zipFile)
+	public void Decompress(DecompressZipToFiles zipFile) throws IOException
 	{
+		byte[] buffer = new byte[1024];
 		
+		ZipInputStream zipInputStream = null;
+		ZipEntry zipEntry = null;
+		FileOutputStream fileOutputStream = null;
+		
+		try
+		{
+			// Assign folder path, if does not exists, create it
+			File folder = new File(zipFile.outputFolder);
+			if(!folder.exists())
+			{
+				folder.mkdir();
+			}
+			
+			// Open stream to read zip
+			zipInputStream = new ZipInputStream(new FileInputStream(zipFile.zipName));
+			zipEntry = zipInputStream.getNextEntry();
+			
+			// Read files from zip
+			while(zipEntry != null)
+			{
+				String fileName = zipEntry.getName();
+				File newFile = new File(zipFile.outputFolder + File.separator + fileName);
+				
+				System.out.println("File unzipped to: " + newFile.getAbsolutePath());
+				
+				new File(newFile.getParent()).mkdirs();
+				
+				fileOutputStream = new FileOutputStream(newFile);
+				
+				int len;
+				while((len = zipInputStream.read(buffer)) > 0)
+				{
+					fileOutputStream.write(buffer, 0, len);
+				}
+				
+				fileOutputStream.close();
+				zipEntry = zipInputStream.getNextEntry();
+			}
+		}
+		finally
+		{
+			zipInputStream.closeEntry();
+			zipInputStream.close();
+		}
 	}
 }
