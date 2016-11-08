f829743514de17fd9452466f8270753bc1fe2e43
Damian <Damian@10.10.23.199>
2016-11-08 10:53:56 +0100

    Test. Dodanie metody do testowania, oraz pliku jar z moimi metodami.

--- Zadanie3/src/test/TestClass.java
+++ Zadanie3/src/test/TestClass.java
@@ -1,5 +1,13 @@
 package test;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import library.CompressFileToZip;
+import library.DecompressZipToFiles;
+import library.FileDownload;
+
 public class TestClass 
 {
 	public static void main(String[] args) 
@@ -16,6 +24,34 @@ public class TestClass
 
 	public static void TestDamian()
 	{
+		URL url;
+		try 
+		{
+			url = new URL("http://www.wp.pl");
+			FileDownload fileDownload = new FileDownload(url, "Plik1.txt");
+			fileDownload.Download();
+		} 
+		catch (MalformedURLException e) 
+		{
+			e.printStackTrace();
+		}
+		catch(IOException e1)
+		{
+			e1.printStackTrace();
+		}
 		
+		CompressFileToZip compressFileToZip = new CompressFileToZip("Plik1.txt", "Zip1");		
+		DecompressZipToFiles decompressFileToZip = new DecompressZipToFiles("Zip1.zip", "Folder");
+		
+		try 	
+		{
+			compressFileToZip.Compress();
+			decompressFileToZip.Decompress();
+			System.out.println("All Good!");
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 }
