8fe226972ae9e75f1553875c7f8070eadfe88e57
fletchPL <wisniewski.maciek1@gmail.com>
2016-11-08 11:05:32 +0100

    male zmiany

--- Zadanie3/src/test/TestClass.java
+++ Zadanie3/src/test/TestClass.java
@@ -20,7 +20,7 @@ import szyfrowanie.SzyfrowanieASE;
 
 public class TestClass 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception 
 	{
 		while(true)
 		{
